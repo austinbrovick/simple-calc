9d891ee6e08c2729836f8e57670931a40a85f956
Austin Brovick <austinbrovick@d-173-250-237-80.dhcp4.washington.edu>
2017-10-10 13:55:22 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Austin Brovick  on 10/10/17.
+//  Copyright © 2017 Austin Brovick . All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
